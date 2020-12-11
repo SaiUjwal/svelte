c56f867cc346effb2fa597cb983f651cf284cf8d
SaiUjwal <saiujwal99@gmail.com>
2020-12-11 18:20:52 +0530

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,101 @@
+<script>
+import { fade } from 'svelte/transition';
+
+    import api from './api'
+    let row=api;
+    let Header = ["id","time","qrCode","name","comment","autoEntry","buildingId","address","category","vehicleNumber","validTill","imageUploaded","reference","active","mobile","contractorId","contractorName","idProof","imageData","gender","updatedOn","createdOn","createdBy","updatedBy","workerProfileVersion","contractorProfileVersion","sequence","categoryname","designationName","categoryId","designationId","fatherName","dob","faceTemplateUploaded","faceTemplate","templateCreateDate","relation","martialStatus","aadharNo","activationDate","pf","uan","prefix","haveEmpId","phoneImage","projectId","stateId","stateName","pinCode","permanentAddress","displayName","projectAddress","projectDisplayName"];
+    </script>
+    
+    <!-- svelte-ignore component-name-lowercase -->  
+    <div class="header">
+    <h1 style="color:blue;">Table generation using svelte</h1>
+    </div>
+            <div class="main">
+                <table>
+                    <thead>
+                        <tr>
+                          {#each Header as header}
+                            <th>{header}</th>
+                          {/each}
+                        </tr>
+                      </thead>
+                    <tbody>
+                        {#each row as item}
+                        <tr>
+                          <td> {item.id }</td>
+                          <td>{item.time}</td>
+                          <td> {item. qrCode} </td>
+                          <td>{item.name}</td>
+                          <td>{item.comment}</td>
+                          <td> {item. autoEntry} </td>
+                          <td>{item.buildingId}</td>
+                          <td> {item. address} </td>
+                          <td>{item.category}</td>
+                          <td>{item.vehicleNumber}</td>
+                          <td> {item. validTill} </td>
+                          <td>{item.imageUploaded}</td>
+                          <td> {item. reference} </td>
+                          <td>{item.active}</td>
+                          <td>{item.mobile}</td>
+                          <td> {item. contractorId} </td>
+                          <td>{item.contractorName}</td>
+                          <td> {item. idProof} </td>
+                          <td>{item.imageData}</td>
+                          <td>{item.gender}</td>
+                          <td> {item. updatedOn} </td>
+                          <td>{item.createdOn}</td>
+                          <td> {item. createdBy} </td>
+                          <td>{item.updatedBy}</td>
+                          <td>{item.workerProfileVersion}</td>
+                          <td> {item. contractorProfileVersion} </td>
+                          <td>{item.sequence}</td>
+                          <td> {item. categoryName} </td>
+                          <td>{item.designationName}</td>
+                          <td>{item.categoryId}</td>
+                          <td> {item. designationId} </td>
+                          <td>{item.fatherName}</td>
+                          <td> {item. dob} </td>
+                          <td>{item.faceTemplateUploaded}</td>
+                          <td>{item.faceTemplate}</td>
+                          <td> {item.templateCreateDate} </td>
+                          <td>{item.relation}</td>
+                          <td> {item. maritalStatus} </td>
+                          <td>{item.aadharNo}</td>
+                          <td>{item.activationDate}</td>
+                          <td> {item. pf} </td>
+                          <td>{item.uan}</td>
+                          <td> {item. prefix} </td>
+                          <td>{item.haveEmpId}</td>
+                          <td>{item.phoneImage}</td>
+                          <td> {item. projectId} </td>
+                          <td>{item.stateId}</td>
+                          <td> {item. stateName} </td>
+                          <td>{item.pinCode}</td>
+                          <td>{item.permanentAddress}</td>
+                          <td> {item. displayName} </td>
+                          <td>{item.projectAddress}</td>
+                          <td> {item. projectDisplayName} </td>
+                          </tr>
+                        {/each}
+                </tbody>
+                </table>
+            </div>
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <html>
+          <style>
+            table {
+              font-family: arial;
+              border-collapse: collapse;
+              width: 150%;
+            }
+          
+            td,th {
+              border: 3px solid #ff3855;
+              text-align: left;
+              padding: 5px;
+            }
+          </style>
+          </html>
+           
+         
+       
